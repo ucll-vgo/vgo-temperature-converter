f1d2686461adb236012647d02cfe6236690fb100
Frederic Vogels <frederic.vogels@gmail.com>
2018-02-14 22:25:14 +0100

    temperature converter

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -27,41 +27,22 @@ namespace View
         }
     }
 
-    public class CelsiusConverter : IValueConverter
+    public class TemperatureConverter : IValueConverter
     {
+        public ITemperatureScale TemperatureScale { get; set; }
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var kelvin = (double)value;
-            var celsius = kelvin - 273.15;
 
-            return celsius.ToString();
+            return this.TemperatureScale.ConvertFromKelvin(kelvin).ToString();
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var celsius = double.Parse((string)value);
-            var kelvin = celsius + 273.15;
+            var temperature = double.Parse((string)value);
 
-            return kelvin;
-        }
-    }
-
-    public class FahrenheitConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var kelvin = (double)value;
-            var fahrenheit = kelvin * 1.8 - 459.67;
-
-            return fahrenheit.ToString();
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var fahrenheit = double.Parse((string)value);
-            var kelvin = (fahrenheit + 459.67) / 1.8;
-
-            return kelvin;
+            return this.TemperatureScale.ConvertToKelvin(temperature);
         }
     }
 }
