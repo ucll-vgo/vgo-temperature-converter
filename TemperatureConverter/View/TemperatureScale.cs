495d18f08b5e6e2ad48dd9d6da7b8908e4e782b0
Frederic Vogels <frederic.vogels@gmail.com>
2018-02-14 14:47:20 +0100

    ITemperatureScale

---
+++ TemperatureConverter/View/TemperatureScale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View
+{
+    public interface ITemperatureScale
+    {
+        string Name { get; }
+
+        double ConvertToKelvin(double temperature);
+
+        double ConvertFromKelvin(double temperature);
+    }
+}
