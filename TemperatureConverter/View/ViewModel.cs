fc4c94f27034a5768802db59006bb0a24c4f191b
Frederic Vogels <frederic.vogels@gmail.com>
2018-02-15 19:53:17 +0100

    TemperatureScaleViewModel

--- TemperatureConverter/View/ViewModel.cs
+++ TemperatureConverter/View/ViewModel.cs
@@ -13,6 +13,13 @@ namespace View
 
         public event PropertyChangedEventHandler PropertyChanged;
 
+        public ConverterViewModel()
+        {
+            this.Kelvin = new TemperatureScaleViewModel(this, new KelvinTemperatureScale());
+            this.Celsius = new TemperatureScaleViewModel(this, new CelsiusTemperatureScale());
+            this.Fahrenheit = new TemperatureScaleViewModel(this, new FahrenheitTemperatureScale());
+        }
+
         public double TemperatureInKelvin
         {
             get
@@ -26,5 +33,42 @@ namespace View
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TemperatureInKelvin)));
             }
         }
+
+        public TemperatureScaleViewModel Kelvin { get; }
+
+        public TemperatureScaleViewModel Celsius { get; }
+
+        public TemperatureScaleViewModel Fahrenheit { get; }
+    }
+
+    public class TemperatureScaleViewModel : INotifyPropertyChanged
+    {
+        private readonly ConverterViewModel parent;
+
+        private readonly ITemperatureScale temperatureScale;
+
+        public TemperatureScaleViewModel(ConverterViewModel parent, ITemperatureScale temperatureScale)
+        {
+            this.parent = parent;
+            this.temperatureScale = temperatureScale;
+
+            this.parent.PropertyChanged += (sender, args) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Temperature)));
+        }
+
+        public string Name => temperatureScale.Name;
+
+        public double Temperature
+        {
+            get
+            {
+                return temperatureScale.ConvertFromKelvin(parent.TemperatureInKelvin);
+            }
+            set
+            {
+                parent.TemperatureInKelvin = temperatureScale.ConvertToKelvin(value);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
