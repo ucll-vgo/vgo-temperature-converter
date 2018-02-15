fbece2f9db7533d1a278fc9385e14e4f2314aea3
Frederic Vogels <frederic.vogels@gmail.com>
2018-02-15 19:54:21 +0100

    removed TemperatureConverter

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -30,23 +30,4 @@ namespace View
             this.DataContext = new ConverterViewModel();
         }
     }
-
-    public class TemperatureConverter : IValueConverter
-    {
-        public ITemperatureScale TemperatureScale { get; set; }
-
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var kelvin = (double)value;
-
-            return this.TemperatureScale.ConvertFromKelvin(kelvin).ToString();
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var temperature = double.Parse((string)value);
-
-            return this.TemperatureScale.ConvertToKelvin(temperature);
-        }
-    }
 }
