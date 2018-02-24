9ba6cb9c9820460d0795e7bcc2e67b44ed7a5616
Frederic Vogels <frederic.vogels@gmail.com>
2018-02-24 13:50:03 +0100

    header

--- TemperatureConverter/View/TemperatureScaleControl.xaml.cs
+++ TemperatureConverter/View/TemperatureScaleControl.xaml.cs
@@ -24,5 +24,14 @@ namespace View
         {
             InitializeComponent();
         }
+
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register("Header", typeof(string), typeof(TemperatureScaleControl), new PropertyMetadata(""));
     }
 }
